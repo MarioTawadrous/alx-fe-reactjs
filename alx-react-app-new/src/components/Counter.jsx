1b9cbe8999012ebf57d3cc9ca0a57467d4d38509
Mario Tawadrous <mario.tawadrous.95@gmail.com>
2025-02-21 16:59:01 +0200

    Update Counter.jsx

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -6,9 +6,9 @@ function Counter() {
   return (
     <div>
       <p>Count is: {count}</p>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
       <button onClick={() => setCount(0)}>Reset</button>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 }
