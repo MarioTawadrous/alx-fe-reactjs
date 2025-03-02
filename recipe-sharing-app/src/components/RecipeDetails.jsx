04ede797e6170a8dda1a5cb5ed269a14fd4a0483
Mario Tawadrous <mario.tawadrous.95@gmail.com>
2025-03-02 22:04:49 +0200

    Add files via upload

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,28 @@
+// RecipeDetails.js
+import React from "react";
+import { useParams } from "react-router-dom";
+import useRecipeStore from "./recipeStore";
+import EditRecipeForm from "./EditRecipeForm";
+import DeleteRecipeButton from "./DeleteRecipeButton";
+
+const RecipeDetails = () => {
+  const { recipeId } = useParams();
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((recipe) => recipe.id === recipeId)
+  );
+
+  if (!recipe) {
+    return <div>Recipe not found</div>;
+  }
+
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      <EditRecipeForm recipe={recipe} />
+      <DeleteRecipeButton recipeId={recipe.id} />
+    </div>
+  );
+};
+
+export default RecipeDetails;
