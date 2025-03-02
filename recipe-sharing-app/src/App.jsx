8dd5804965582ad014292d7cdbcc8f9e77b3484c
Mario Tawadrous <mario.tawadrous.95@gmail.com>
2025-03-02 21:12:10 +0200

    Add files via upload

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import RecipeList from "./compnents/RecipeList";
+
+function App() {
+  return (
+    <>
+      Hello
+      <RecipeList />
+    </>
+  );
+}
+
+export default App;
