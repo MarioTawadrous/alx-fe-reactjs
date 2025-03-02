7b44e5f7c07ba47d267b75589d335b541a7ea54e
Mario Tawadrous <mario.tawadrous.95@gmail.com>
2025-03-02 22:20:02 +0200

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,27 @@
-// RecipeList component
-import { useRecipeStore } from "../recipeStore";
+// src/components/RecipeList.jsx
+import React from 'react';
+import useRecipeStore from '../stores/recipeStore';
 
-function RecipeList() {
-  const recipes = useRecipeStore((state) => state.recipes);
+const RecipeList = () => {
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
 
   return (
     <div>
-      {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <h2>Recipes</h2>
+      {filteredRecipes.length > 0 ? (
+        <ul>
+          {filteredRecipes.map((recipe) => (
+            <li key={recipe.id}>
+              <h3>{recipe.title}</h3>
+              <p>{recipe.description}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No recipes found.</p>
+      )}
     </div>
   );
-}
+};
 
 export default RecipeList;
