3d21be21c117f02d4f3376149fc1ccf50ac2895e
Mario Tawadrous <mario.tawadrous.95@gmail.com>
2025-03-02 21:22:05 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import RecipeList from "./compnents/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
     <>
-      Hello
       <RecipeList />
+      <AddRecipeForm />
     </>
   );
 }
