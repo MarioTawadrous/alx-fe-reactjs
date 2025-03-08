6056ea37e9bd82a82e44792deadb846cda34a0cf
Mario Tawadrous <mario.tawadrous.95@gmail.com>
2025-03-08 19:47:23 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,6 +5,30 @@ const useRecipeStore = create((set, get) => ({
   recipes: [], // Initial list of recipes
   favorites: [], // Array of favorite recipe IDs
   recommendations: [], // Array of recommended recipes
+  searchTerm: '', // State for the search term
+  filteredRecipes: [], // State for filtered recipes
+
+  // Action to add a new recipe
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, { ...recipe, id: Date.now().toString() }],
+    })),
+
+  // Action to set the list of recipes (e.g., from an API)
+  setRecipes: (recipes) => set({ recipes }),
+
+  // Action to update the search term
+  setSearchTerm: (term) => set({ searchTerm: term }),
+
+  // Action to filter recipes based on the search term
+  filterRecipes: () => {
+    const { recipes, searchTerm } = get();
+    set({
+      filteredRecipes: recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+      ),
+    });
+  },
 
   // Action to add a recipe to favorites
   addFavorite: (recipeId) =>
